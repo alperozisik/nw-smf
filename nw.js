9913d4050cdcbec8e5a73bf89706f4712de78258
Alper Özışık <alper.ozisik@smartface.io>
2016-10-25 16:24:27 +0000

    added lib

---
+++ nw.js
@@ -0,0 +1,43 @@
+/* globals */
+
+
+var nw = module.exports = {
+    baseURL: "",
+    services: {},
+    factory: null, //defered
+    commonHeaders: {},
+    registerService: registerService,
+    proxy: "",
+    ignoreSSLErrors: false,
+    requestSuccessValidator: requestSuccessValidator,
+    onActivityStart: null,
+    onActivityEnd: null
+};
+
+
+nw.factory = require("./lib/nw-factory")(nw);
+
+function registerService(serviceDescription) {
+    if (typeof serviceDescription !== "object")
+        throw Error("serviceDescription must be an object");
+    if (typeof serviceDescription.name !== "string")
+        throw Error("serviceDescription.name must be a string");
+    if (typeof serviceDescription.path !== "string" && typeof serviceDescription.url !== "string")
+        throw Error("serviceDescription.path OR serviceDescription.url must be string");
+    if(typeof serviceDescription.method !== "string")
+        throw Error("serviceDescription.method must be a string");
+    serviceDescription.method = serviceDescription.method.toUpperCase();
+    nw.services[serviceDescription.name] = Object.assign(serviceDescription, {
+        header: {},
+        query: {},
+        body: {}
+    });
+
+}
+
+function requestSuccessValidator(status) {
+    status = parseInt(status, 10);
+    if(status >= 200 && status < 400)
+        return true;
+    return false;
+}
